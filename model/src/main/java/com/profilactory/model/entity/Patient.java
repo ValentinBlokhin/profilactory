b577fa62faca51527efda1ae48cb818f7deca8b5
valentin <blokhinvalentin@gmail.com>
2014-03-16 19:26:16 +0200

    first commit. Created project profilactory and added model module, but does not filled yet.

---
+++ model/src/main/java/com/profilactory/model/entity/Patient.java
@@ -0,0 +1,11 @@
+package com.profilactory.model.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Валентин
+ * Date: 16.03.14
+ * Time: 19:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Patient {
+}
